e356186f58cd89e004b68dbe707f38a0c39917df
chbu <chbu@dtu.dk>
2020-03-02 12:01:51 +0100

    Concurrency

--- src/main/java/CustomThread.java
+++ src/main/java/CustomThread.java
@@ -1,16 +1,6 @@
 public class CustomThread extends Thread {
     @Override
     public void run() {
-        try {
-            for (int i = 0; i < 10 ; i++) {
-                Thread.sleep((int) (Math.random() * 5000));
-                System.out.println("Hello no." + i + " from Thread: " + Thread.currentThread().getName());
-            }
-            System.out.println(Thread.currentThread().getName() + " is done!");
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
+        System.out.println("Hello from Thread: " + Thread.currentThread().getName());
     }
 }
---
+++ src/main/java/RunnableRunner.java
@@ -0,0 +1,13 @@
+public class RunnableRunner {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Hello from " + Thread.currentThread().getName());
+                }
+            }).start();
+        }
+    }
+}
