ae535dd191561bc280a0ca024a25b56bf32125e2
cbudtz <chrbudtz@gmail.com>
2020-03-06 18:42:02 +0100

    Nu med billede!

--- src/main/java/FXAppGuiController.java
+++ src/main/java/FXAppGuiController.java
@@ -1,6 +1,15 @@
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
 public class FXAppGuiController {
-    public void Drawlines(MouseEvent mouseEvent) {
+    public Image image;
+    public ImageView imageView;
+
+    public void Drawlines(MouseEvent mouseEvent) throws IOException {
     }
 }
---
+++ src/main/java/FXMLApp.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class FXMLApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXAppGui.fxml"));
+        AnchorPane anchorPane= loader.load();
+        primaryStage.setScene(new Scene(anchorPane));
+        primaryStage.show();
+
+    }
+}
